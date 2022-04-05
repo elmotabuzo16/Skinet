6139356adae317001b7990ea05bf03a502baaf32
Elmo <elmotabuzojr.ust@gmail.com>
2022-04-05 20:31:50 +0800

    S4 - Adding images

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,7 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Called middleware
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
