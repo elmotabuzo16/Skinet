23158519e0d915168c7f1fea503ad4956c45bdca
Elmo <elmotabuzojr.ust@gmail.com>
2022-04-10 20:09:20 +0800

    S9 - Adding Paging Header Component in Angular

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-
-
 @NgModule({
   declarations: [NavBarComponent],
   imports: [
-  CommonModule
+    CommonModule
   ],
   exports: [NavBarComponent]
 })
